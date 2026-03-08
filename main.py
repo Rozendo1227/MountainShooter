51b8d6458a3542aa5cbded739defc7f1132131ef
Rozendo1227 <joserozendojunior@hotmail.com>
2026-03-08 06:34:20 -0300

    Add print statement for quitting event

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ while True:
     #Check for all events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            print('Quiting....')
             pygame.quit() # Close Window
             quit() # End pygame
 
